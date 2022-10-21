45b1f5d4f93c80d7ec5dc7cd174c69e7b750c78a
affan880 <syedaffan880@gmail.com>
2022-10-22 01:18:08 +0530

    code: Scaffold home page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Routes from './routes'
+import NavBar from './components/navBar/navBar'
+
+const App = () => {
+  return (
+    <div>
+    <NavBar />
+    <Routes/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './loader.css'
+const Loader = () => {
+  return (
+      <div id="loader-overlay">
+          <div class="loader">
+              <div class="loader-inner"></div>
+          </div>
+      </div>
+  )
+}
+
+export default Loader
