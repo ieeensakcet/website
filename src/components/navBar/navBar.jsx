3eec2b7a5c83ac6500c8139377cfe66566dbef7f
affan880 <syedaffan880@gmail.com>
2022-10-30 23:48:39 +0530

    code : scafolded webinars page

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -3,7 +3,23 @@ import './navBar.css'
 import SBlogo from '../../assets/img/SB-logo.png'
 import { Link } from "react-router-dom";
 
+import '../../styles/css/icofonts.min.css'
 const NavBar = () => {
+  const [burger_class, setBurgerClass] = useState("burger-bar unclicked")
+  const [menu_class, setMenuClass] = useState("menu hidden")
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
+
+  const updateMenu = () => {
+    if (!isMenuClicked) {
+      setBurgerClass("burger-bar clicked")
+      setMenuClass("menu visible")
+    }
+    else {
+      setBurgerClass("burger-bar unclicked")
+      setMenuClass("menu hidden")
+    }
+    setIsMenuClicked(!isMenuClicked)
+  }
   return (
     <div >
       <nav className="navbar navbar-default navbar-fixed navbar-transparent dark bootsnav on no-full no-border">
@@ -14,48 +30,50 @@ const NavBar = () => {
           </ul>
         </div>
         <div className="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu"> <i class="tr-icon "></i> </button>
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu"> <i class="tr-icon ion-android-menu"></i> </button>
           <div className="logo" > <a href="index.html"> <img className="logo logo-display" src={SBlogo} alt="" /> <img className="logo logo-scrolled" src={SBlogo} alt="" /> </a> </div>
         </div>
         <div className="container-fluid" style={{
           width: "100%",
         }}>
-            <div className="collapse navbar-collapse" id="navbar-menu" >
-              <ul className="nav navbar-nav navbar-center" data-in="fadeIn" data-out="fadeOut">
-                <li>
-                  <Link to="/">Home</Link>
+
+
+          <div className="collapse navbar-collapse" id="navbar-menu" >
+            <ul className="nav navbar-nav navbar-center" data-in="fadeIn" data-out="fadeOut">
+              <li>
+                <Link to="/">Home</Link>
               </li>
               <li>
-                  <Link to="/aboutUs">About</Link>
+                <Link to="/aboutUs">About</Link>
               </li>
               <li className="dropdown"><a href="#" className="dropdown-toggle" data-toggle="dropdown">Events</a>
                 <ul className="dropdown-menu">
-                  <li><a href="blog-grid.html">Webinars</a></li>
+                  <li><Link to="/webinars">Webinars</Link></li>
                   <li><a href="blog-classNameic.html">Quizes</a></li>
                   <li><a href="blog-fullwidth.html">Competitions</a></li>
                   <li><a href="blog-masonry.html">Workshop</a></li>
                 </ul>
               </li>
-                <li className="dropdown"><a href="#" className="dropdown-toggle" data-toggle="dropdown">Chapters</a>
-                  <ul className="dropdown-menu">
+              <li className="dropdown"><a href="#" className="dropdown-toggle" data-toggle="dropdown">Chapters</a>
+                <ul className="dropdown-menu">
                   <li><Link to={"/cs"} >Computer Society</Link></li>
                   <li><Link to={"/ras"}>Robotics and Automation Society</Link></li>
                   <li><Link to="/wie">Women in Engineering</Link></li>
                   <li><Link to={"/cas"} >Circuits and Systems</Link></li>
                   <li><Link to={"sps"} >Signal Processing Society</Link></li>
-                  </ul>
-                </li>
+                </ul>
+              </li>
 
-                <li>
-                  <Link to="/team">Team</Link>
-                </li>
-                <li>
-                  <Link to="/joinUs">Join us</Link>
-                </li>
-                
-              </ul>
-            </div>
-        </div>  
+              <li>
+                <Link to="/team">Team</Link>
+              </li>
+              <li>
+                <Link to="/joinUs">Join us</Link>
+              </li>
+
+            </ul>
+          </div>
+        </div>
       </nav>
     </div>
   )
---
+++ src/pages/webinars/webinars.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import LandingScreen from '../../components/chaptersCustomCom/LandingScreen/landingScreen'
+function webinars() {
+    return (
+        <div>
+            <section class="white-bg">
+                <div class="container">
+                    <div class="row blog-style-01">
+                        <div class="col-md-4 col-sm-6 col-xs-12">
+                            <div class="post animation-move-top mb-30">
+                                <div class="post-img"> <img class="img-responsive" src="assets/images/post/post-01.jpg" alt="" /> </div>
+                                <div class="post-info all-padding-40">
+                                    <h5>January 15, 2018</h5>
+                                    <h3><a href="blog-grid.html">How These Different Book Covers Reflect the Design</a></h3>
+                                    <h6><a class="viewmore" href="#.">view more</a></h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row blog-style-01">
+                        <div class="col-md-4 col-sm-6 col-xs-12">
+                            <div class="post animation-move-top mb-30">
+                                <div class="post-img"> <img class="img-responsive" src="assets/images/post/post-01.jpg" alt="" /> </div>
+                                <div class="post-info all-padding-40">
+                                    <h5>January 15, 2018</h5>
+                                    <h3><a href="blog-grid.html">How These Different Book Covers Reflect the Design</a></h3>
+                                    <h6><a class="viewmore" href="#.">view more</a></h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row blog-style-01">
+                        <div class="col-md-4 col-sm-6 col-xs-12">
+                            <div class="post animation-move-top mb-30">
+                                <div class="post-img"> <img class="img-responsive" src="assets/images/post/post-01.jpg" alt="" /> </div>
+                                <div class="post-info all-padding-40">
+                                    <h5>January 15, 2018</h5>
+                                    <h3><a href="blog-grid.html">How These Different Book Covers Reflect the Design</a></h3>
+                                    <h6><a class="viewmore" href="#.">view more</a></h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row blog-style-01">
+                        <div class="col-md-4 col-sm-6 col-xs-12">
+                            <div class="post animation-move-top mb-30">
+                                <div class="post-img"> <img class="img-responsive" src="assets/images/post/post-01.jpg" alt="" /> </div>
+                                <div class="post-info all-padding-40">
+                                    <h5>January 15, 2018</h5>
+                                    <h3><a href="blog-grid.html">How These Different Book Covers Reflect the Design</a></h3>
+                                    <h6><a class="viewmore" href="#.">view more</a></h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row blog-style-01">
+                        <div class="col-md-4 col-sm-6 col-xs-12">
+                            <div class="post animation-move-top mb-30">
+                                <div class="post-img"> <img class="img-responsive" src="assets/images/post/post-01.jpg" alt="" /> </div>
+                                <div class="post-info all-padding-40">
+                                    <h5>January 15, 2018</h5>
+                                    <h3><a href="blog-grid.html">How These Different Book Covers Reflect the Design</a></h3>
+                                    <h6><a class="viewmore" href="#.">view more</a></h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              </section>  
+        </div>
+  )
+}
+
+export default webinars
